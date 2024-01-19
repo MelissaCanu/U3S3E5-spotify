0cc9d485b8e21be18c6f5bd5c72df82a8bea4c4a
MelissaCanu <melissamcanu@gmail.com>
2024-01-19 12:18:11 +0100

    displaying results, need to implement bootstrap style

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,87 +1,146 @@
 import React from "react";
 import "../App.css";
 
-const albumCard = function (songInfo) {
-	return `
-      <div class="col text-center" id=${songInfo.id}>
-          <img class="img-fluid" src=${
-						songInfo.album.cover_medium
-					} alt="track" />
-        <p>
-            Track: "${
-							songInfo.title.length < 16
-								? `${songInfo.title}`
-								: `${songInfo.title.substring(0, 16)}...`
-						}"<br>
-            Artist: ${songInfo.artist.name}
-        </p>
-      </div>`;
-};
-
-const search = async (event) => {
-	event.preventDefault();
-	let div = document.querySelector("#searchResults .row");
-	div.innerHTML = "";
-	let searchQuery = document.querySelector("#searchField").value;
-
-	if (searchQuery.length > 2) {
-		document.querySelector("#searchResults").style.display = "block";
-
-		try {
-			let response = await fetch(
-				`https://striveschool-api.herokuapp.com/api/deezer/search?q=${searchQuery}`,
-				{
-					method: "GET",
-					headers: {
-						"X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
-						"X-RapidAPI-Key":
-							"9d408f0366mshab3b0fd8e5ecdf7p1b09f2jsne682a1797fa0",
-					},
-				}
-			);
-			if (response.ok) {
-				let result = await response.json();
-				let { data } = result;
-				for (let x = 0; x < data.length; x++) {
-					div.innerHTML += albumCard(data[x]);
-				}
-			} else {
-				throw new Error("error in search");
-			}
-		} catch (err) {
-			console.log("error", err);
-		}
-	} else {
-		document.querySelector("#searchResults").style.display = "none";
-	}
+const AlbumCard = ({ songInfo }) => {
+	return (
+		<div className="col text-center" id={songInfo.id}>
+			<img
+				className="img-fluid"
+				src={songInfo.album.cover_medium}
+				alt="track"
+			/>
+			<p>
+				Track:{" "}
+				{songInfo.title.length < 16
+					? `${songInfo.title}`
+					: `${songInfo.title.substring(0, 16)}...`}
+				<br />
+				Artist: {songInfo.artist.name}
+			</p>
+		</div>
+	);
 };
 
 const Sidebar = () => {
+	const albumCard = (songInfo) => (
+		<AlbumCard key={songInfo.id} songInfo={songInfo} />
+	);
+
+	const handleSearch = async (event) => {
+		event.preventDefault();
+		let div = document.querySelector("#searchResults .row");
+		div.innerHTML = "";
+		let searchQuery = document.querySelector("#searchField").value;
+
+		if (searchQuery.length > 2) {
+			document.querySelector("#searchResults").style.display = "block";
+
+			try {
+				let response = await fetch(
+					`https://striveschool-api.herokuapp.com/api/deezer/search?q=${searchQuery}`,
+					{
+						method: "GET",
+						headers: {
+							"X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+							"X-RapidAPI-Key":
+								"9d408f0366mshab3b0fd8e5ecdf7p1b09f2jsne682a1797fa0",
+						},
+					}
+				);
+				if (response.ok) {
+					let result = await response.json();
+					let { data } = result;
+					data.forEach((song) => {
+						div.innerHTML += albumCard(song);
+					});
+				} else {
+					throw new Error("Error in search");
+				}
+			} catch (err) {
+				console.error("Error", err);
+			}
+		} else {
+			document.querySelector("#searchResults").style.display = "none";
+		}
+	};
+
 	return (
 		<nav
 			className="navbar navbar-expand-md fixed-left justify-content-between"
 			id="sidebar"
 		>
-			{/* The rest of your Sidebar component code... */}
-			<form className="input-group mt-3" onSubmit={(event) => search(event)}>
-				<input
-					type="text"
-					className="form-control"
-					id="searchField"
-					placeholder="Search"
-					aria-label="Search"
-					aria-describedby="basic-addon2"
-				/>
-				<div className="input-group-append">
-					<button
-						className="btn btn-outline-secondary btn-sm h-100"
-						type="submit"
-					>
-						GO
-					</button>
+			<div className="container flex-column align-items-start">
+				<a className="navbar-brand" href="index.html">
+					<img
+						src="assets/logo/logo.png"
+						alt="Spotify Logo"
+						width="131"
+						height="40"
+					/>
+				</a>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-toggle="collapse"
+					data-target="#navbarNavAltMarkup"
+					aria-controls="navbarNavAltMarkup"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+					<div className="navbar-nav">
+						<ul>
+							<li>
+								<a
+									className="nav-item nav-link d-flex align-items-center"
+									href="#"
+								>
+									<i className="bi bi-house-door-fill"></i>&nbsp; Home
+								</a>
+							</li>
+							<li>
+								<a
+									className="nav-item nav-link d-flex align-items-center"
+									href="#"
+								>
+									<i className="bi bi-book-fill"></i>&nbsp; Your Library
+								</a>
+							</li>
+							<li>
+								<form className="input-group mt-3" onSubmit={handleSearch}>
+									<input
+										type="text"
+										className="form-control"
+										id="searchField"
+										placeholder="Search"
+										aria-label="Search"
+										aria-describedby="basic-addon2"
+									/>
+									<div className="input-group-append">
+										<button
+											className="btn btn-outline-secondary btn-sm h-100"
+											type="submit"
+										>
+											GO
+										</button>
+									</div>
+								</form>
+							</li>
+						</ul>
+					</div>
 				</div>
-			</form>
-			{/* The rest of your Sidebar component code... */}
+			</div>
+			<div className="nav-btn">
+				<button className="btn signup-btn" type="button">
+					Sign Up
+				</button>
+				<button className="btn login-btn" type="button">
+					Login
+				</button>
+				<a href="#">Cookie Policy</a> |<a href="#"> Privacy</a>
+			</div>
 		</nav>
 	);
 };
