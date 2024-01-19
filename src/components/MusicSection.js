ac2e9062037e7947ad7a1b26653b111d00918a65
MelissaCanu <melissamcanu@gmail.com>
2024-01-19 16:43:11 +0100

    white h2

--- src/components/MusicSection.js
+++ src/components/MusicSection.js
@@ -49,7 +49,7 @@ const MusicSection = ({ title, artistName, querySelector }) => {
 		<div className="row">
 			<div className="col-10">
 				<div id={querySelector}>
-					<h2>{title}</h2>
+					<h2 style={{ color: "white" }}>{title}</h2>
 					<div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 imgLinks py-3">
 						{songs.map((song) => (
 							<AlbumCard
